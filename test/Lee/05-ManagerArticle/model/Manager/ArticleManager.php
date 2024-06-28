e771a09f8eb74826a6a9959cc3805d71b7ff5b2d
leerlandais <leerlandais@gmail.com>
2024-06-28 07:21:51 +0100

    query : OK - Needs mapping and displaying

--- test/Lee/05-ManagerArticle/model/Manager/ArticleManager.php
+++ test/Lee/05-ManagerArticle/model/Manager/ArticleManager.php
@@ -171,6 +171,39 @@ class ArticleManager implements InterfaceManager{
         }
         
     }
+    public function selectAllWithCatAndComm() : array | string
+    {
+        $sql  = "SELECT A.*,
+                COALESCE(comm_count, 0) AS comm_count,
+                COALESCE(cats, '') AS cats
+                FROM article A
+                LEFT JOIN (
+                        SELECT C.article_article_id,
+                        COUNT(C.comment_id) AS comm_count
+                        FROM comment C
+                        GROUP BY C.article_article_id
+                        ) AS comm 
+                        ON A.article_id = comm.article_article_id
+                        LEFT JOIN (
+                                SELECT AHC.article_article_id,
+                                GROUP_CONCAT(CAT.category_name SEPARATOR ', ') AS cats
+                                FROM article_has_category AHC
+                        LEFT JOIN category CAT 
+                        ON AHC.category_category_id = CAT.category_id
+                        GROUP BY AHC.article_article_id
+                        ) AS cat ON A.article_id = cat.article_article_id";
+
+
+        $query = $this->runQuery($sql);
+        if (!is_array($query)) throw new Exception("Something went wrong with the query");
+        $result = [];
+        foreach($query as $value){
+            $result[] = new ArticleMapping($value);
+        }
+        return $result;
+    }
 
 }
+
+
 
--- test/Lee/05-ManagerArticle/view/article/article.home.view.php
+++ test/Lee/05-ManagerArticle/view/article/article.home.view.php
@@ -11,11 +11,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$title?>></title>
+    <title><?=$title?></title>
 </head>
 <body>
 <?php include 'menu.article.view.php' ?>
-
+<?php
+var_dump($crazilyNestedSql);
+?>
 <section class="relative py-16">
     <div class="w-full mb-12 px-4">
         <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded
