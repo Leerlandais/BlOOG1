d1c0b7dd9ca8948e16b9f856706c53cabad3793e
leerlandais <leerlandais@gmail.com>
2024-06-28 07:03:15 +0100

    sql : OK

--- test/Lee/05-ManagerArticle/view/article/test.php
+++ test/Lee/05-ManagerArticle/view/article/test.php
@@ -30,3 +30,30 @@ GROUP BY
     A.article_id;";
 
 
+$sql = "SELECT 
+    A.*,
+    COALESCE(comm_count, 0) AS comm_count,
+    COALESCE(cats, '') AS cats
+FROM 
+    article A
+LEFT JOIN (
+    SELECT 
+        C.article_article_id,
+        COUNT(C.comment_id) AS comm_count
+    FROM 
+        comment C
+    GROUP BY 
+        C.article_article_id
+) AS comm ON A.article_id = comm.article_article_id
+LEFT JOIN (
+    SELECT 
+        AHC.article_article_id,
+        GROUP_CONCAT(CAT.category_name SEPARATOR ', ') AS cats
+    FROM 
+        article_has_category AHC
+    LEFT JOIN 
+        category CAT ON AHC.category_category_id = CAT.category_id
+    GROUP BY 
+        AHC.article_article_id
+) AS cat ON A.article_id = cat.article_article_id;
+"
